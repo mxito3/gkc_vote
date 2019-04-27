d986d1c79c67f30c8eedb0513789606b2fe41521
mxito3 <1477311729@qq.com>
2019-04-27 16:29:14 +0800

    update

--- main.py
+++ main.py
@@ -186,8 +186,8 @@ def who_i_vote_to():
 
 
 
-@app.route('/sign_transfer',methods=['Post'])
-def sign_transfer():
+@app.route('/transfer',methods=['Post'])
+def transfer():
     try:
         form  = Result_Util.get_json_from_request(request)
     except CommonError as e:
@@ -242,59 +242,59 @@ def sign_transfer():
 
 
 
-@app.route('/transfer',methods=['Post'])
-def transfer():
-    try:
-        form  = Result_Util.get_json_from_request(request)
-    except CommonError as e:
-        result = Result_Factory.generate_result(status=False,message=e.args[0])
-        return result
+# @app.route('/transfer',methods=['Post'])
+# def transfer():
+#     try:
+#         form  = Result_Util.get_json_from_request(request)
+#     except CommonError as e:
+#         result = Result_Factory.generate_result(status=False,message=e.args[0])
+#         return result
     
-    # raw_amount = owner
-    from_account = address_util.toChecksumAddress(owner)
-    try:
-        field_name = 'to'
-        raw_to =  Result_Util.get_field_from_json(form,field_name)
-    except CommonError as e:
-        result = Result_Factory.generate_result(status=False,message=e.args[0])
-        return result
-    to = address_util.toChecksumAddress(raw_to)
-    # if from_account == to:
-    #     result = Result_Factory.generate_result(status=False,message=Error_Messages.From_And_to_Same_Error)
-    #     return result
-    if not (from_account and to):
-        result = Result_Factory.generate_result(status=False,message=Error_Messages.Address_Length_Error)
-        return result
-    password = owner_password
+#     # raw_amount = owner
+#     from_account = address_util.toChecksumAddress(owner)
+#     try:
+#         field_name = 'to'
+#         raw_to =  Result_Util.get_field_from_json(form,field_name)
+#     except CommonError as e:
+#         result = Result_Factory.generate_result(status=False,message=e.args[0])
+#         return result
+#     to = address_util.toChecksumAddress(raw_to)
+#     # if from_account == to:
+#     #     result = Result_Factory.generate_result(status=False,message=Error_Messages.From_And_to_Same_Error)
+#     #     return result
+#     if not (from_account and to):
+#         result = Result_Factory.generate_result(status=False,message=Error_Messages.Address_Length_Error)
+#         return result
+#     password = owner_password
 
-    try:
-        field_name = 'amount'
-        raw_amount =  Result_Util.get_field_from_json(form,field_name)
-    except CommonError as e:
-        result = Result_Factory.generate_result(status=False,message=e.args[0])
-        return result
+#     try:
+#         field_name = 'amount'
+#         raw_amount =  Result_Util.get_field_from_json(form,field_name)
+#     except CommonError as e:
+#         result = Result_Factory.generate_result(status=False,message=e.args[0])
+#         return result
 
-    if raw_amount is None:
-        result = Result_Factory.generate_result(status=False,message=Error_Messages.Transfer_Amount_None_Error)
-        return result
-    amount = raw_amount
-    if isinstance(amount,str):
-        result = Result_Factory.generate_result(status=False,message=Error_Messages.Transfer_Amount_type_Error)
-        return result
-    print("参数是  {} {} {} {}".format(from_account,password,to,amount))
-    # if from_account and password,to,amount
-    try:
-        details= eth_util.transfer_ether(from_account,password,to,amount)        
-        if details is None:
-            result = Result_Factory.generate_result(status=False,message=Error_Messages.No_Enough_Balance)
-            return result    
-    except Exception as e:
-        print(e)
-        result = Result_Factory.generate_result(status=False,message=e.args[0])
-        return result
-    else:
-        result = Result_Factory.generate_result(status=True,data=details)
-        return result
+#     if raw_amount is None:
+#         result = Result_Factory.generate_result(status=False,message=Error_Messages.Transfer_Amount_None_Error)
+#         return result
+#     amount = raw_amount
+#     if isinstance(amount,str):
+#         result = Result_Factory.generate_result(status=False,message=Error_Messages.Transfer_Amount_type_Error)
+#         return result
+#     print("参数是  {} {} {} {}".format(from_account,password,to,amount))
+#     # if from_account and password,to,amount
+#     try:
+#         details= eth_util.transfer_ether(from_account,password,to,amount)        
+#         if details is None:
+#             result = Result_Factory.generate_result(status=False,message=Error_Messages.No_Enough_Balance)
+#             return result    
+#     except Exception as e:
+#         print(e)
+#         result = Result_Factory.generate_result(status=False,message=e.args[0]['message'])
+#         return result
+#     else:
+#         result = Result_Factory.generate_result(status=True,data=details)
+#         return result
 
 
 @app.route('/get_balance')
